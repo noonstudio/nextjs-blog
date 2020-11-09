5a93058b57399684100a71275115d8cac320d9f1
Alex <alex@noon.studio>
2020-11-09 18:07:21 +0000

    commit new files

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,44 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPostsForHome } from '../lib/api'
+import { CMS_NAME } from '../lib/constants'
 
-import blogStyles from '../styles/posts.module.css';
+export default function Index({ allPosts: { edges }, preview }) {
+  const heroPost = edges[0]?.node
+  const morePosts = edges.slice(1)
 
-import { getAllPosts } from '../lib/api';
-
-export async function getStaticProps() {
-  const allPosts = await getAllPosts();
-  return {
-    props: {
-      allPosts
-    }
-  };
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.featuredImage?.node}
+              date={heroPost.date}
+              author={heroPost.author?.node}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
 }
 
-
-export default function Home({ allPosts: { edges } }) {
-  return (
-    <Layout>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-
-      <section>
-        {edges.map(({ node }) => (
-          <div className={blogStyles.listitem} key={node.id}>
-            <div className={blogStyles.listitem__content}>
-              <h2>{node.title}</h2>
-              <Link href={`/posts/${node.slug}`}>
-                <a>Read more ></a>
-              </Link>
-            </div>
-          </div>
-        ))}
-      </section>	
-    </Layout>
-  )
+export async function getStaticProps({ preview = false }) {
+  const allPosts = await getAllPostsForHome(preview)
+  return {
+    props: { allPosts, preview },
+  }
 }
