7e001ce88c0f70443c664bfb20f26ba37bcc902a
Alex <alex@noon.studio>
2020-11-10 15:34:55 +0000

    revert header.js

--- components/header.js
+++ components/header.js
@@ -10,12 +10,14 @@ export default function Header({
 }) {
 	
   return (
+	  <>
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
         <a className="hover:underline">{title}</a>
       </Link>
       .
     </h2>
+	  <>
   )
 }
 
